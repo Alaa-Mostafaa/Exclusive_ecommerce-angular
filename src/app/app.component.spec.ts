49019b23ade4d55fa12c321e2d3140139aea8847
Alaa-Mostafaa <alan.mustafa.2002@gmail.com>
2024-11-04 18:09:02 +0200

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'final_ecommerce'`, () => {
+  it(`should have as title 'ecommerce'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('final_ecommerce');
+    expect(app.title).toEqual('ecommerce');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, final_ecommerce');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ecommerce');
   });
 });
